2168a571f4a8851ff6443d0e0b3eed4876bd63d9
Shelbyrp <77222563+Shelbyrp@user.noreply.github.com>
2021-05-16 13:10:28 +0800

    Set up the API routes.

--- server.js
+++ server.js
@@ -1,20 +1,42 @@
-// Import the required modules
+// Dependencies
 const express = require("express");
-const fs = require("fs");
 const path = require("path");
+const fs = require("fs");
 const http = require("http");
-var PORT = process.env.PORT || 8080;
 
+// Sets up the Express App
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-app.get("/", function(req, res) {
-    res.json(path.join(__dirname, "public/assets/index.html"));
-  });
-  
+// Routes
 
+// Basic route that sends the user first to the AJAX Page
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public', 'index.html')));
 
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public', 'notes.html')));
+
+// Displays all notes
+app.get('/api/notes', (req, res) => res.json(notes));
+
+app.get("/api/notes", (request, response) => {
+
+    fs.readFile(path.join(__dirname, "db", "db.json"), 'utf8', (err, jsonString) => {
+        if (err) {
+            console.log("File read failed:", err)
+            return
+        }
+        console.log('File data:', jsonString)
+        response.json(JSON.parse(jsonString));
+        // json.parse
+    })
+})
+
+// Server listening confirmation
 app.listen(PORT, () => {
     console.log(`Server is listening on PORT ${PORT}`);
 });
